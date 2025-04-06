49a99bbf3c34b8b0be0f21c86bfda927b34f1ad4
KIRAN-03 <121012948+KIRAN-03@users.noreply.github.com>
2025-04-06 17:12:40 +0530

    second commit

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 const DBconnection = async () =>{
-    const MONODB_URI = `mongodb://kirankorrapolu:WoiuCi0cdoq6zAgP@file-sharing-shard-00-00.i5pno.mongodb.net:27017,file-sharing-shard-00-01.i5pno.mongodb.net:27017,file-sharing-shard-00-02.i5pno.mongodb.net:27017/?ssl=true&replicaSet=atlas-gppn81-shard-0&authSource=admin&retryWrites=true&w=majority&appName=file-sharing`;
+    const MONODB_URI = ``;
     try {
         await mongoose.connect(MONODB_URI);
         console.log('MongoDB connected');
